637c9ec39f64e141c6c84be4c4ea26798b36b8a5
vccorimanya <raulccorimanyaalfaro@gmail.com>
2022-02-14 14:48:05 -0500

    add mekos

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const text = document.querySelector('#push_me')
-
 const button = document.querySelector('.pushable')
+const img = document.querySelector('#img')
 
-const palabras = ['Michi', 'Gato', 'Neko']
-
+const palabras = ['Michi', 'Gato', 'Neko', 'Michi', 'Gato', 'Neko']
+const rutas = [
+  './feliz.png',
+  './cara-de-gato.png',
+  './gato-burmilla.png',
+  './gato-con-raquetas-de-nieve.png',
+  './gato-negro.png',
+  './gato.png',
+]
 /* fetch('https://api.paperquotes.com/apiv1/quotes/?lang=es')
 .then(response => response.json())
 .then(data => {
@@ -15,9 +22,11 @@ function getRandom(min, max){
 }
 
 button.addEventListener('click', ()=> {
-  const random= getRandom(0,3)
+  const random= getRandom(0,6)
   let news = palabras[random]
+  let ruta = rutas[random]
   text.innerText = news
+  img.src = ruta
 })
 
 /* text.innerText = `${contador}` */
